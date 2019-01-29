2d8f2f507d7be207771bc0bbd482f98004521be6
Asen Plesnev <as.plesnev@gmail.com>
2019-01-29 20:52:55 +0200

    Data Variables

---
+++ Data Types And Variables/Greeting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = Console.ReadLine();
+            string LastName = Console.ReadLine();
+            byte age = byte.Parse(Console.ReadLine());
+            Console.WriteLine($"Hello, {FirstName} {LastName}. You are {age} years old.");
+        }
+    }
+}
